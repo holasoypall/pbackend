6cdb8ee177cede9cc74041d5b8c7ab57d5c7514a
holasoypall <dosestuches@gmail.com>
2022-10-31 16:11:46 +0100

    First commit

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
-
     closeAlert();
-
 });
 
 function closeAlert(){
-
-    const bot = document.querySelector("#alert_close");
-    const alert = document.querySelector("#alert");
-    bot.addEventListener("click", ()=>{
-        alert.parentNode.removeChild(alert);
-    })
-
+    if(document.querySelector("#alert_close") != null){
+        let bot = document.querySelector("#alert_close");
+        let alert = document.querySelector("#alert");
+        bot.addEventListener("click", ()=>{
+            alert.parentNode.removeChild(alert);
+        })
+    }
 }
